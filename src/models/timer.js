c919ec2eef58e63ec1a0754fe1334b265e6f43a0
DuStark <dustar@dustark.local>
2017-07-08 23:06:43 +0800

    [update]

--- src/models/timer.js
+++ src/models/timer.js
@@ -3,5 +3,43 @@
  */
 
 class Timer {
+    static currentId
+    constructor(title, interval) {
+        this.type = "Timer"
+        this.id = ++currentId
+        this.title = title
+        this.available = true
+        this.createAt = new Date()
+        this.remindAt = this.createAt + interval * 60
+    }
 
+    create() {
+
+    }
+
+    delete() {
+
+    }
+
+    remove() {
+
+    }
+
+    remind() {
+
+    }
+
+    //
+
+    stop() {
+
+    }
+
+    delay() {
+
+    }
+
+    output() {
+
+    }
 }
--- src/models/todo.js
+++ src/models/todo.js
@@ -1,3 +1,49 @@
 /**
  * Created by dustar on 2017/7/8.
  */
+
+/**
+ * Created by dustar on 2017/7/8.
+ */
+
+class Todo {
+    static currentId
+    constructor(title) {
+        this.type = "Todo"
+        this.id = ++currentId
+        this.title = title
+        this.available = true
+        this.createAt = new Date()
+    }
+
+    create() {
+
+    }
+
+    delete() {
+
+    }
+
+    //
+
+    stop() {
+
+
+    }
+
+    output() {
+
+    }
+}
+
+class TodosQueue {
+    items = []
+
+    count () {
+        return items.length
+    }
+
+    switch () {
+
+    }
+}
